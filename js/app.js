b6b642931f0193d95bdeecaf2c0b332a4d9d974a
arcodev <chris_arco0794@hotmail.com>
2021-06-10 12:00:02 -0500

    Aplicando efecto sobre imagen al cambia de tema

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
+//Variables
 const boton = document.querySelector('#boton');
+const body = document.querySelector('body');
 /*Con macthMedia, detectaremos el tema que tiene el equipo*/
 const configUser = window.matchMedia('(prefers-color-scheme: dark)');
+
+/*LocalStorage para guardar la configuracion del tema al dar click en el boton*/
+const localConfig = localStorage.getItem('tema');
+if (localConfig === 'dark') {
+    body.classList.toggle('dark-theme');
+} else if (localConfig === 'light') {
+    body.classList.toggle('light-theme');
+}
+
 boton.addEventListener('click', () => {
-    console.log(configUser.matches);
-    if(configUser.matches) {
+    //console.log(configUser.matches);
+    //variable para guardar el tema en el key del localStorage
+    let colorTema;
+    if (configUser.matches) {
         //entramos con modo oscuro
-        document.body.classList.toggle('light-theme');
+        body.classList.toggle('light-theme');
+        //Utilizando el operador ternario
+        colorTema = body.classList.contains('light-theme') ? 'light' : 'dark';
     } else {
-        document.body.classList.toggle('dark-theme');
+        body.classList.toggle('dark-theme');
+        colorTema = body.classList.contains('light-theme') ? 'dark' : 'light';
     }
+    localStorage.setItem('tema', colorTema);
 });
